4ba2775b14914965285cf134d3247b0f84040e40
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-05-20 17:37:52 -0300

    test: restaurantlist should display restaurants

--- src/components/RestaurantList/__tests__/RestaurantList.test.js
+++ src/components/RestaurantList/__tests__/RestaurantList.test.js
@@ -1,17 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import { RestaurantList } from '../RestaurantList';
 
+const NOOP = () => {};
 describe('RestaurantList should', () => {
-  it('render correctly', () => {
-    render(<RestaurantList loadRestaurants={() => {}} />);
-    expect(screen.getByRole('list')).toBeInTheDocument();
-  });
-
   it('load restaurants on first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
 
     render(<RestaurantList loadRestaurants={loadRestaurants} />);
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('display the restaurants', () => {
+    const restaurants = [
+      { id: 1, name: 'Sushi Place' },
+      { id: 2, name: 'Pizza Place' }
+    ];
+
+    render(<RestaurantList loadRestaurants={NOOP} restaurants={restaurants} />);
+
+    const restaurantListItems = screen.getAllByRole('listitem');
+    expect(restaurantListItems.length).toBe(restaurants.length);
+
+    restaurantListItems.forEach((r, index) => {
+      expect(r.textContent).toBe(restaurants[index].name);
+    });
   });
 });

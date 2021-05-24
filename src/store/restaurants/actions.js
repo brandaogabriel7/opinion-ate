84ad3ef4e95147e41947af5ed522bcc9cc26c68d
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-05-24 11:40:29 -0300

    feat: loadrestaurants action should store the restaurants

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,1 +1,14 @@
-export const loadRestaurants = () => () => {};
+const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+
+const loadRestaurants = () => (dispatch, getState, api) => {
+  api.loadRestaurants().then((records) => {
+    dispatch(storeRestaurants(records));
+  });
+};
+
+const storeRestaurants = (records) => ({
+  type: STORE_RESTAURANTS,
+  records
+});
+
+export { STORE_RESTAURANTS, loadRestaurants };
